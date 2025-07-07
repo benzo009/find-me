e735a2f3b6e610c881c1baa012e8307cd2301911
benzo009 <104455011+benzo009@users.noreply.github.com>
2025-07-07 22:54:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('myLink').addEventListener('click', function(event) {
+    event.preventDefault(); // prevent default link behavior
+    // Replace this URL with your target website
+    window.location.href = 'https://x.com/beng__g?s=21';
+  });
+  
