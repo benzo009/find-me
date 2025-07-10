3b5d9f606e882d7ef4cef496535594235bb61d85
benzo009 <104455011+benzo009@users.noreply.github.com>
2025-07-10 23:26:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ document.getElementById('myLink').addEventListener('click', function(event) {
     // Replace this URL with your target website
     window.location.href = 'https://x.com/beng__g?s=21';
   });
-  
+   async function fetchVisitorCount() {
+      try {
+        const response = await fetch('https://8bvl8r4xn7.execute-api.us-east-1.amazonaws.com/production');  // Replace with your actual API URL
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        document.getElementById('counter').textContent = data.count;
+      } catch (error) {
+        console.error('Error fetching visitor count:', error);
+        document.getElementById('counter').textContent = 'N/A';
+      }
+    }
+
+    fetchVisitorCount();
