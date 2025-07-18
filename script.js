04e4b5289ce17b8231b8bf6218234488bfb5b032
benzo009 <104455011+benzo009@users.noreply.github.com>
2025-07-18 23:55:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('myLink').addEventListener('click', function(event) {
+document.getElementById('twitter').addEventListener('click', function(event) {
     event.preventDefault(); // prevent default link behavior
     // Replace this URL with your target website
     window.location.href = 'https://x.com/beng__g?s=21';
