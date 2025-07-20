867275ad5c64f36f8922de7e19ed75311c1d0880
benzo009 <104455011+benzo009@users.noreply.github.com>
2025-07-20 17:31:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-document.getElementById('twitter').addEventListener('click', function(event) {
-    event.preventDefault(); // prevent default link behavior
-    // Replace this URL with your target website
-    window.location.href = 'https://x.com/beng__g?s=21';
-  });
-   async function fetchVisitorCount() {
-      try {
-        const response = await fetch('https://8bvl8r4xn7.execute-api.us-east-1.amazonaws.com/production');  // Replace with your actual API URL
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        document.getElementById('counter').textContent = data.count;
-      } catch (error) {
-        console.error('Error fetching visitor count:', error);
-        document.getElementById('counter').textContent = 'N/A';
-      }
+function redirectTo(url) {
+  window.open(url, '_blank');
+}
+window.onload = () => {
+  fetchVisitorCount();
+};
+  
+
+async function fetchVisitorCount() {
+  try {
+    const response = await fetch('https://8bvl8r4xn7.execute-api.us-east-1.amazonaws.com/production');  
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
 
-    fetchVisitorCount();
+    const result = await response.json(); 
+    const data = JSON.parse(result.body); 
+
+    document.getElementById('counter').textContent = data.count;
+  } catch (error) {
+    console.error('Error fetching visitor count:', error);
+    document.getElementById('counter').textContent = 'N/A';
+  }
+}
+window.onload = () => {
+  fetchVisitorCount();
+  setInterval(fetchVisitorCount, 10000); // refresh every 10 seconds
+};
+
