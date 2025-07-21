b28a7653cc87923c5342bcab3719d984ccdc0df8
benzo009 <104455011+benzo009@users.noreply.github.com>
2025-07-21 18:18:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+async function fetchVisitorCount() {
+  try {
+    const response = await fetch('https://8bvl8r4xn7.execute-api.us-east-1.amazonaws.com/production', {
+      method: 'GET',
+      credentials: 'include'  // Allow browser to send and receive cookies
+    });
 
-
+    const data = await response.json();
+    document.getElementById('counter').textContent = data.count;
   } catch (error) {
     console.error("Error fetching visitor count:", error);
     document.getElementById('counter').textContent = 'N/A';
   }
 }
+
